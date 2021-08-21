3b6cc591c06e6af2ece7c13bc599910368b672d4
shree8371 <65858871+shree8371@users.noreply.github.com>
2021-08-21 21:36:21 +0530

    Add files via upload

---
+++ src/Comp1.js
@@ -0,0 +1,14 @@
+function Comp1(){
+
+    let name = "Welcome";
+
+    return(
+        
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
+
+}
+
+export default Comp1;
---
+++ src/Comp2.js
@@ -0,0 +1,14 @@
+function Comp2(){
+
+    let name="Lets Upgrade Team";
+
+    return(
+    <div>
+
+        <h2>{name}</h2>
+
+    </div>
+    )
+}
+
+export default Comp2;
